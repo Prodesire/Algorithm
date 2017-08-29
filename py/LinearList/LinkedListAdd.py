200bf527a912bb758dc32cdddd349f04400b11ce
Prodesire <Prodesire@yeah.net>
2017-08-29 23:36:59 +0800

    [py]完成链表部分翻转

---
+++ py/LinearList/LinkedListAdd.py
@@ -0,0 +1,58 @@
+"""
+给定两个链表，分表表示两个非负整数。它们的数字逆序储存在链表中，
+且每个节点只储存一个数字，计算两个数的和，并且返回和的链表头指针。
+如：
+    输入： 2->4->3, 5->6->4
+    输出： 7->0->8
+"""
+from random import randint
+from Structure import Node
+
+
+def add(phead1, phead2):
+    psum = Node(0)
+    ptail = psum
+    p1 = phead1.next
+    p2 = phead2.next
+    carry = 0
+
+    while p1 and p2:
+        value = p1.value + p2.value + carry
+        carry = value / 10
+        value %= 10
+        ptail.next = Node(value)
+        ptail = ptail.next
+
+        p1 = p1.next
+        p2 = p2.next
+
+    p = p1 if p1 else p2
+    while p:
+        value = p.value + carry
+        carry = value / 10
+        ptail.next = Node(value)
+        ptail = ptail.next
+        p = p.next
+
+    if carry:
+        ptail.next = Node(carry)
+    return psum
+
+
+if __name__ == '__main__':
+    phead1 = Node(0)
+    for i in range(6):
+        p = Node(randint(0, 9) % 10)
+        p.next = phead1.next
+        phead1.next = p
+
+    phead2 = Node(0)
+    for i in range(9):
+        p = Node(randint(0, 9) % 10)
+        p.next = phead2.next
+        phead2.next = p
+
+    print(phead1)
+    print(phead2)
+    psum = add(phead1, phead2)
+    print(psum)
---
+++ py/LinearList/LinkedListReverse.py
@@ -0,0 +1,36 @@
+"""
+给定一个链表，翻转该链表从 m 到 n 的位置。
+要求直接翻转而非申请空间。
+如：
+    输入： 1->2->3->4->5, m=2, n=4
+    输出： 1->4->3->2->5
+"""
+from random import randint
+from Structure import Node
+
+
+def reverse(phead, m, n):
+    pcur = phead
+    for i in range(0, m-1):
+        pcur = pcur.next
+
+    ppre = pcur
+    pcur = pcur.next
+
+    for i in range(m, n):
+        pnext = pcur.next
+        pcur.next = pnext.next
+        pnext.next = ppre.next
+        ppre.next = pnext
+
+
+if __name__ == '__main__':
+    phead = Node(0)
+    for i in range(10):
+        p = Node(randint(0, 99) % 10)
+        p.next = phead.next
+        phead.next = p
+
+    print(phead)
+    reverse(phead, 4, 8)
+    print(phead)
