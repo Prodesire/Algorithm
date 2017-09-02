34fbb9fced63b76fb635209ef815ff5d9d6929b6
Prodesire <Prodesire@yeah.net>
2017-09-02 23:00:26 +0800

    [py]完善字符串循环移位和LCS

--- py/string/CycleShift.py
+++ py/string/CycleShift.py
@@ -1,24 +1,29 @@
 """
-求两个字符串的最长公共子序列的长度。
+给定一个字符串S[0...N-1]，要求把S的前k个字符移动到S的尾部。
+要求时间复杂度为O(n)，空间复杂度为O(1)。
 如：
-    字符串'abcbdab'和'bdcaba'的LCS长度是4。
+    把字符串'abcdef'前两个字符'a'、'b'移动到字符串尾部得到'cdefab'
+思路：
+    根据数学原理 (A'B')' = BA，就是先转置A，再转置B，再转置A'B'，就能得到BA
 """
 
 
-def lcs(s1, s2):
-    len1 = len(s1) + 1
-    len2 = len(s2) + 1
-    c = [[0 for _ in range(len2)] for _ in range(len1)]
-    for i in range(1, len1):
-        for j in range(1, len2):
-            if s1[i - 1] == s2[j - 1]:
-                c[i][j] = c[i - 1][j - 1] + 1
-            else:
-                c[i][j] = max(c[i - 1][j], c[i][j - 1])
-    return c[len1 - 1][len2 - 1]
+def cycle_shift(s, n, k):
+    k %= n
+    reverse(s, 0, k - 1)
+    reverse(s, k, n - 1)
+    reverse(s, 0, n - 1)
+
+
+def reverse(s, from_, to):
+    while from_ < to:
+        s[from_], s[to] = s[to], s[from_]
+        from_ += 1
+        to -= 1
 
 
 if __name__ == '__main__':
-    s1 = 'abcbdab'
-    s2 = 'bdcaba'
-    print(s1, s2, lcs(s1, s2))
+    s = list('abcdef')
+    print(s)
+    cycle_shift(s, len(s), 2)
+    print(s)
